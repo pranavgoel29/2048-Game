c5e68e60d3f05bf6ce6c10ff1daff6a1480d817b
pranavgoel29 <pranavgoel29@gmail.com>
2023-06-08 10:44:03 +0530

    Modify: updated the tag of header to header from div

--- src/Components/GameInfo.tsx
+++ src/Components/GameInfo.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 const GameInfo = (gameScore: any) => {
-
   return (
-    <>
-      <div className="gameInfoSection">
-        <h1>2048</h1>
-        <div className="scoreSection">
-          <div className="scoreSectionCard">
-            <p>Score: {gameScore.currentScore}</p>
-          </div>
-          <div className="scoreSectionCard">
-            <p>Best: {gameScore.bestScore}</p>
-          </div>
+    <nav className="gameInfoSection">
+      <h1>2048</h1>
+      <div className="scoreSection">
+        <div className="scoreSectionCard">
+          <p>Score: {gameScore.currentScore}</p>
+        </div>
+        <div className="scoreSectionCard">
+          <p>Best: {gameScore.bestScore}</p>
         </div>
       </div>
-    </>
+    </nav>
   );
 };
 
